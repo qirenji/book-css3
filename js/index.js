5902035bdda3e28c06455744e54ebe5447dfa24d
qirenji <q290187113@qq.com>
2017-08-27 12:25:57 +0800

    add note

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 var books = document.querySelectorAll('.bookPage');
+// 设置书页
 for(var i=0; i<books.length; i++){
 	setTransform(books[i],'translate3d(0px, 0px, '+(-i)+'px)')
 }
 
+// transform属性兼容
 function setTransform(e,v){
 	var s = e.style;
 	s.webkitTransform = 
@@ -19,14 +21,18 @@ window.onload = function(){
 	var rotY=0;
 	var flag = true;
 	var timer;
+
+	//鼠标摁下 
 	bookWrap.onmousedown = function(ev){
 		var cx = ev.clientX;
 		var cy = ev.clientY;
+		// 点击标志位
 		flag = true;
 		timer = setTimeout(function(){
 			flag = false;
 		},200);
 
+		// 鼠标移动
 		document.onmousemove = function(ev){
 				var x = ev.clientX;
 				var y = ev.clientY;
@@ -34,10 +40,13 @@ window.onload = function(){
 				rotY -= disX/2;
 				setTransform(bookWrap,'rotateY('+rotY+'deg)');
 		}
+
+		//鼠标抬起 
 		document.onmouseup = function(ev){
 			document.onmousemove = document.onmouseup =null;
 			clearTimeout(timer);
 			console.log(flag);
+			// 点击执行操作
 			if(flag){
 				var x =ev.pageX - bookWrap.offsetLeft;
 				if(x<Math.floor(bookWrap.offsetWidth/2)){
@@ -51,6 +60,7 @@ window.onload = function(){
 		ev.preventDefault();
 	}
 
+	// 前一页
 	function previousPage(){
 		if(currentPage>0){
 			currentPage--;
@@ -58,6 +68,7 @@ window.onload = function(){
 		}
 	}
 
+	// 后一页
 	function nextPage(){
 		if(currentPage < books.length){
 			setTransform(books[currentPage],'translate3d(0px,0px,'+currentPage+'px) rotateY(-170deg)');
